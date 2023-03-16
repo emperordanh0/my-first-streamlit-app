644bbb34594030aed3f7fafd8223251cad4d1ec5
Danh_005 <51956280+emperordanh0@users.noreply.github.com>
2023-03-16 14:23:29 +0700

    Update temperature-revenue.py

--- temperature-revenue.py
+++ temperature-revenue.py
@@ -1,14 +1,7 @@
 import pickle
 import streamlit as st
-import matplotlib.pyplot as plt
-import numpy as np
 
 model = pickle.load(open('model.pickle', "rb"))
-if st.button('Go'):
-  x_line = np.array([np.min(x), np.max(x)]).reshape(-1,1)
-  y_line = model.predict(x_line)
-  plt.scatter(x, y)
-  plt.plot(x_line, y_line, color = 'red')
-  plt.xlabel('Temperature')
-  plt.ylabel('Revenue')
-  plt.show()
+x = st.number_input('Input Temperature')
+if st.predict('Predict'):
+  st.success(model.predict(x))
