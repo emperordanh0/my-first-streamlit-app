a1010445e0809b1d1d5c7fcaf52cdcb533d8f398
Danh_005 <51956280+emperordanh0@users.noreply.github.com>
2023-03-16 14:25:08 +0700

    Update temperature-revenue.py

--- temperature-revenue.py
+++ temperature-revenue.py
@@ -4,4 +4,4 @@ import streamlit as st
 model = pickle.load(open('model.pickle', "rb"))
 x = st.number_input('Input Temperature')
 if st.button('Predict'):
-  st.success(model.predict(x))
+  st.success(model.predict(x).reshape(-1,1))
