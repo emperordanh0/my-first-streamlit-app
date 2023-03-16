8b6fc3d567e58a3ebb1df823502e487e4ec0d42f
Danh_005 <51956280+emperordanh0@users.noreply.github.com>
2023-03-16 17:47:04 +0700

    Update temperature-revenue.py

--- temperature-revenue.py
+++ temperature-revenue.py
@@ -4,7 +4,7 @@ import numpy as np
 
 model = pickle.load(open('model.pickle', "rb"))
 x = np.array(st.number_input('Input Temperature'))
-x = x.reshape(-1,1)
+x = x.reshape(1,-1)
 if st.button('Predict'):
   y = model.predict(x)
   st.success(y)
